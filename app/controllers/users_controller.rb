61b8dd184e0fdd3284f7722d2c799f15cc4f6b42
Apple0410 <ngominh041097@gmail.com>
2022-10-21 13:26:35 +0700

    Huge commitment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def create
@@ -26,9 +30,9 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information has successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
-            render "update"
+            render "edit"
         end
     end
 
