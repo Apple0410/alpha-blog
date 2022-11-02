98657871e896e04d7b75325a36c7c1f6277d17f9
Apple0410 <ngominh041097@gmail.com>
2022-11-02 16:22:44 +0700

    Wrap up alpha-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,66 +1,62 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :destroy, :edit]
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     before_action :require_user, except: [:show, :index]
     before_action :require_same_user, only: [:edit, :update, :destroy]
-
+  
     def show
-        set_article
     end
-
+  
     def index
-        @articles = Article.paginate(page: params[:page], per_page: 5)
-    end 
-
+      @articles = Article.paginate(page: params[:page], per_page: 5)
+    end
+  
     def new
-        @article = Article.new
+      @article = Article.new
     end
-
+  
     def edit
-        set_article
     end
-    
+  
     def create
-        @article = Article.new(article_params)
-        @article.user = current_user
-        if @article.save
-            flash[:notice] = "Article was created successfully."
-            redirect_to @article
-        else
-            render 'new'
-        end
+      @article = Article.new(article_params)
+      @article.user = current_user
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article
+      else
+        render 'new'
+      end
     end
-
+  
     def update
-        set_article
-        if @article.update(article_params)
-            flash[:notice] = "Article was created successfully."
-            redirect_to @article
-        else
-            render 'edit'
-        end
+      if @article.update(article_params)
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article
+      else
+        render 'edit'
+      end
     end
-
+  
     def destroy
-        set_article
-        @article.destroy
-        redirect_to articles_path # redirect to the artciles page
+      @article.destroy
+      redirect_to articles_path
     end
-
-    private # use inside controller
-    
+  
+    private
+  
     def set_article
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-
+  
     def article_params
-        params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
-
+  
     def require_same_user
-        if current_user != @article.user && !current_user.admin?
-            flash[:alert] = "You can only edit or delete your own user"
-            redirect_to @artciles
-        end
+      if current_user != @article.user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own article"
+        redirect_to @article
+      end
     end
-
-end
+  
+  end
